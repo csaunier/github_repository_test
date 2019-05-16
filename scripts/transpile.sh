e54c14096cdad9967f0b97fe11c550afd5f8b1f6
Cédric Saunier <cedric.saunier@jobteaser.com>
2019-05-16 15:21:22 +0200

    config app and components

---
+++ scripts/transpile.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo -e "\e[1;94m# - Clean dist directory\e[0m"
+
+rm -rf dist/
+mkdir dist
+mkdir dist/lib
+mkdir dist/module
+
+echo -e "\e[92m-> Done\e[0m"
+
+echo -e "\e[1;94m# - Build commonjs components library\e[0m"
+
+NODE_ENV=commonjs \
+npx babel \
+    src/components \
+    --out-dir dist/lib \
+    --copy-files \
+    --plugins @babel/plugin-transform-modules-commonjs \
+    --ignore "__snapshots__","src/lib/**/*.test.js"
+
+echo -e "\e[92m-> Done\e[0m"
+
+echo -e "\e[1;94m#- Build es6 components library\e[0m"
+
+NODE_ENV=esm \
+npx babel \
+    src/components \
+    --out-dir dist/module \
+    --copy-files \
+    --ignore "__snapshots__","src/lib/**/*.test.js"
+
+echo -e "\e[92m-> Done\e[0m"
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./button.css"
+
+const Button = () => {
+  return <button className={styles.button}>My first button</button>
+}
+
+export default Button
